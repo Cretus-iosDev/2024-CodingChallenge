3ac7cbcb83d80dbc5dffac158010ff1d1997bb34
Rutik Maraskolhe <84556881+Cretus-iosDev@users.noreply.github.com>
2024-01-02 09:32:09 +0530

    Add files via upload

---
+++ Logic/conditionalAndLoop.cpp
@@ -0,0 +1,104 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+  // int n;
+  // cin>>n;
+ // cout<<"Value of n is - "<<n<<endl;
+
+  
+  // if a is positive
+  /*
+  if(n>0) {
+    cout<<"A is Positivie"<<endl;
+  }
+  else {
+    cout<<"A is negative"<<endl;
+  }
+  
+
+  int a,b;
+  cout<<"Enter the value of a"<<endl;
+  cin>>a;
+  cout<<"Enter the value of b"<<endl;
+  cin>>b;
+  if(a>b){
+    cout<<"A is greater"<<endl;
+  }
+  if(b>a){
+    cout<<"B is greater"<<endl;
+  }
+
+  
+
+
+
+ int a;
+ cout<<"enter the value of a"<<endl;
+ cin>>a;
+ 
+ if(a>0){
+    cout<<"A is positive";
+ }
+ else if(a<0){
+    cout<<"a is negative";
+ }
+ else{
+    cout<<"a is 0";
+ }
+ */
+    /*
+    //questn-1
+    int a=9;
+    if(a==9){
+        cout<<"niney";
+    }
+
+    if(a>0){
+        cout<<"positive";
+    }
+    else{
+        cout<<"negative";
+    }
+    */
+
+   /*
+   //qustn-2
+   int a=2;
+   int b=a+1;
+   if((a=3)==b){
+        cout<<a;
+   }
+   else{
+        cout<<a+1;
+   }
+   */
+
+  //qustn-3
+  /*
+  int a=24;
+  if(a>20){
+    cout<<"rutik";
+  }
+  else if(a==24){
+    cout<<"rezz";
+  }
+  else{
+    cout<<"asdasd";
+  }
+  cout<<a;
+  */
+
+    //qustn-4
+  
+    char c;
+    cout<<"enter value of c - "<<endl;
+    cin>>c;
+    if(c>='a' && c<='z'){
+        cout<<"lower-case";
+    } else if(c>='A' && c<='Z'){
+        cout<<"upper-case";
+    } else if(c>='0' && c<='1'){
+        cout<<"integer";
+    }
+}
