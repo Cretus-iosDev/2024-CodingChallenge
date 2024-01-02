34aa1c810269f5db76985a50a4044adee6af5ef8
Rutik Maraskolhe <84556881+Cretus-iosDev@users.noreply.github.com>
2024-01-02 09:44:38 +0530

    Add files via upload

---
+++ Logic/DataTypes.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    // Integer
+    int a = 123;
+    cout << a << endl;
+
+    // Character
+    char b = 'v';
+    cout << b << endl;
+
+    // Bool
+    bool bl = true;
+    cout << bl << endl;
+
+    // float
+    float  f = 1.2;
+    cout << f << endl;
+
+    // double
+    double d = 1.23;
+    cout << d << endl;
+
+    // sizeof
+    int size = sizeof(d);
+    cout << "size of a is:- " << size << endl;
+
+}
---
+++ Logic/TypeCasting.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a  = 'a';
+    cout << a << endl;
+
+    char ch  =  99;
+    cout << ch << endl;
+
+    char ch1 = 123456;
+    cout << ch1 << endl;
+
+    // How to sotre '-ve' number
+    unsigned int c = 122;
+    unsigned int d = -122;
+    cout << c << endl;
+    cout << d << endl;
+
+
+    // Operators
+    float f = 2.0/5;
+    cout << f << endl;
+
+    // relational operator
+    int x = 21;
+    int n = 44;
+    bool first = (x==n);
+    cout << first << endl;
+
+    bool second = (x>n);
+    cout << second << endl;
+     
+    bool third = (x>=n);
+    cout << third << endl;
+
+    bool fourth = (x<=n);
+    cout << fourth << endl;
+
+    bool fifth = (x!=n);
+    cout << fifth << endl;
+
+    bool sixth = (x<n);
+    cout << sixth << endl;
+
+
+    // Logical operator
+    int h = 0;
+    cout << !h << endl;
+
+    
+
+    
+    
+}
