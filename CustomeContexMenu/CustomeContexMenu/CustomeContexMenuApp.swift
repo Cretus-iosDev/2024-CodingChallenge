48b63456a440edac34ce11d6eb69a5f15c26f34d
Rutik Maraskolhe <84556881+Cretus-iosDev@users.noreply.github.com>
2024-01-06 22:24:24 +0530

    Add files via upload

---
+++ CustomeContexMenu/CustomeContexMenu/CustomeContexMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomeContexMenuApp.swift
+//  CustomeContexMenu
+//
+//  Created by Dr. Shrikant Maraskolhe on 06/01/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomeContexMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
